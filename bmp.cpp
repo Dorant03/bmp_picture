a2ca52770dd5e2c71b4f955fb8cdf75ba5f8baa7
Dorant03 <wolodymyrsydorko@gmail.com>
2017-09-22 08:51:21 +0300

    commit encoded

--- bmp.cpp
+++ bmp.cpp
@@ -18,4 +18,28 @@ int main()
 		cout<<"2.Decode messages"<<endl;
         
 		int choice=1;
+
+                while(choice)
+		{
+		cout<<"Your choice: "; cin>>choice;
+		if(choice==1)
+		{
+			file1 = fopen("file.bmp", "r+b");
+            file2 = fopen("file1.bmp", "w+b");
+
+			fread(&header,sizeof(header),1,file1);                     //файловий заголовок(BITMAPFILEHEADER)
+            fwrite(&header, sizeof(header), 1, file2);  
+            fread(&bmiHeader,sizeof(bmiHeader),1,file1);               //заголовок зображення(BITMAPINFOHEADER)
+            fwrite(&bmiHeader, sizeof(bmiHeader), 1, file2);  
+
+		    string messages;
+		    cout<<"Enter messages:"<<endl;
+			cin.get();
+		    getline(cin,messages);
+
+		    int len=messages.size(); int size=len;
+		    int k=8;
+		    int number_ascii=(int)(messages[len-size]);
+                }
+                }
 }
