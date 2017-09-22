6b7f2e4d8c684cf2527a93957b25aa7b59c8d499
Dorant03 <wolodymyrsydorko@gmail.com>
2017-09-22 08:49:02 +0300

    commit menu

--- bmp.cpp
+++ bmp.cpp
@@ -1,9 +1,21 @@
-#include <iostream>
-using namespace std;
+#include<iostream>
+#include <windows.h>;
+#include <string>
+ using namespace std;
+
 int main()
-{                        
-                          cout << "Hello!"<<endl;
-	system("pause");
-	return 0;
-	
+{   
+        // Структури файла
+        BITMAPFILEHEADER header;
+        BITMAPINFOHEADER bmiHeader;
+		//дані про пікселі
+        RGBTRIPLE rgb; 
+		//файли для читання і запису
+        FILE * file1, * file2;
+ 
+		cout<<"Menu:"<<endl;
+		cout<<"1.Encode messages"<<endl;
+		cout<<"2.Decode messages"<<endl;
+        
+		int choice=1;
 }
